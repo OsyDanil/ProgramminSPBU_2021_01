8f27fb0e8846ee9975e598d85b6b681df7c4f616
OsyDanil <st095234@student.spbu.ru>
2021-09-19 19:43:18 +0300

    Last up

--- 2021.09.18_task1/Project_10/Source.cpp
+++ 2021.09.18_task1/Project_10/Source.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	int a, b;
-	cin >> a >> b; 
-   
-
-
+	cin >> a >> b;
+	int x = abs(a - b);
+	int ans = (a + b + x) / 2;
+	cout << ans;
 	return EXIT_SUCCESS;
 }
