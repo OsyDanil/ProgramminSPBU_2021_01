1d01d1d155e5fb2e8214b4cbf945ec249ef2bc5b
OsyDanil <st095234@student.spbu.ru>
2021-09-21 19:54:06 +0300

    5 up

--- 2021.09.18_task1/Project_08/Source.cpp
+++ 2021.09.18_task1/Project_08/Source.cpp
@@ -5,11 +5,10 @@ int main()
 {
 	int h1 = 0;
 	int m1 = 0;
-	int	s1 = 0;
-	int	h2 = 0;
-	int	m2 = 0;
-	int	s2 = 0;
-
+	int s1 = 0;
+	int h2 = 0;
+	int m2 = 0;
+	int s2 = 0;
 	cin >> h1 >> m1 >> s1 >> h2 >> m2 >> s2;
 	int t1 = h1 * 3600 + m1 * 60 + s1;
 	int t2 = h2 * 3600 + m2 * 60 + s2;
